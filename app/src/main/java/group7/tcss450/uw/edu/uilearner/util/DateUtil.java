8d3c3d39078bc35684ce014b078a9a831903f881
Myles Haynes <myleschaynes@gmail.com>
2017-11-29 14:40:42 -0800

    add getRestOfDay for AgendaView

--- app/src/main/java/group7/tcss450/uw/edu/uilearner/util/DateUtil.java
+++ app/src/main/java/group7/tcss450/uw/edu/uilearner/util/DateUtil.java
@@ -38,6 +38,28 @@ public class DateUtil {
         return strings;
     }
 
+
+    /**
+     *
+     * @param year year, 1 indexed
+     * @param month 0 indexed
+     * @param day 1 indexed
+     * @param hour 1 indexed
+     * @param minute 1 indexed
+     * @return 0 index is current time formatted to date, 1 index is the end of the day.
+     */
+    public static String[] getRestOfDay(int year, int month, int day, int hour, int minute) {
+        String[] strs = new String[2];
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month, day, hour, minute);
+        Date start = cal.getTime();
+        cal.set(year, month, day, 23, 59, 59);
+        Date end = cal.getTime();
+        strs[0] = FORMAT.format(start);
+        strs[1] = FORMAT.format(end);
+        return strs;
+    }
+
     public static String getRfcString(int year, int month, int day, int hour, int minute) {
         Calendar cal = Calendar.getInstance();
         cal.set(year, month, day, hour, minute);
